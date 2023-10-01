05fb0a8fbfb723223ad45e80779d71878d48772e
bishal-dhakal <bishaldhakal95@yahoo.com>
2023-10-01 14:57:17 +0545

    relationships

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,22 +1,34 @@
 from pydantic import BaseModel
+from typing import List
 
-class Blog(BaseModel):
-    title:str
-    body:str
 
-class ShowBlog(Blog):
-    class Config():
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+class Blog(BlogBase):
+    class Config:
         orm_mode = True
 
 class User(BaseModel):
-    name:str
-    email:str
-    password:str
+    name: str
+    email: str
+    password: str
+
 
 class ShowUser(BaseModel):
-    name : str
-    email:str
-    class Config():
+    name: str
+    email: str
+    blogs: List[Blog] =[]
+
+    class Config:
         orm_mode = True
 
-    
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+    class Config:
+        orm_mode = True
